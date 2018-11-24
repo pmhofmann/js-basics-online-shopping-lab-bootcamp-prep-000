fde9b6860410825755fdbde4d879025512109bc6
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2018-11-24 17:25:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,14 +28,14 @@ else {
 var purchases = "In your cart, you have ";
 for (var i = 0; i <cart.length;i++);
 if (i === cart.length-1 && cart.length === 1){
-        list += (`${cart[i].itemName} at $${cart[i].itemPrice}.`);
+        purchases += (`${cart[i].itemName} at $${cart[i].itemPrice}.`);
       } else if (i === cart.length-1 && cart.length>1){
-        list += (`and ${cart[i].itemName} at $${cart[i].itemPrice}.`);
+        purchases += (`and ${cart[i].itemName} at $${cart[i].itemPrice}.`);
       } else {
-        list += (`${cart[i].itemName} at $${cart[i].itemPrice}, `);
+        purchases += (`${cart[i].itemName} at $${cart[i].itemPrice}, `);
       }
     }
-    return list;
+    return purchases;
 }
 function total() {
   // write your code here
